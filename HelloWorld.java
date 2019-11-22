5733902bb1530d255045e8c5c65a15c70940a26e
jaegerbluemix <40160135+jaegerbluemix@users.noreply.github.com>
2019-11-22 11:11:44 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+/** 
+ * The HelloWorldApp class implements an application that simply prints "Hello, Devops Folks!!!" to standard output.
+ */
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, Devops Folks!!!");
+  }
+}
